ea6c2a0dabb8fb5e16cbfe53e0b2c34dae4a29cc
Akira Kobori <private.beats@gmail.com>
2021-01-14 20:19:15 +0900

    Refactor

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,61 +1,41 @@
 const BRACKETS: [[char; 2]; 3] = [['[', ']'], ['{', '}'], ['(', ')']];
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    println!("string: {}", string);
-    let mut string = string.trim();
+    let string = string.trim();
     let begin_pos = string
         .chars()
         .position(|c| c == '[' || c == '{' || c == '(');
-    if begin_pos.is_some() {
-        let begin_pos = begin_pos.unwrap();
-        let begin_char = string
-            .chars()
-            .skip(begin_pos)
-            .take(1)
-            .collect::<Vec<char>>()[0];
-        println!("begin_char: {}  begin_pos: {}", begin_char, begin_pos);
-        let end_char = BRACKETS.iter().find(|b| b[0] == begin_char).unwrap()[1];
-        if has_end_char(string, begin_pos) {
-            println!("has end char");
-            return false;
-        }
-        let end_pos = get_end_pos(string, begin_char, begin_pos, end_char);
-        if end_pos.is_none() {
-            return false;
-        }
-        let end_pos = end_pos.unwrap();
-        println!("end_char: {}  end_pos: {}", end_char, end_pos);
-        let inline_string = &string[begin_pos + 1..end_pos];
-        println!("inline_string: {}", inline_string);
-        if !brackets_are_balanced(inline_string) {
-            return false;
-        }
-        string = &string[end_pos + 1..string.len()];
-        return brackets_are_balanced(string);
-    } else {
-        println!("begin_pos is none");
-        let end_pos = string
-            .chars()
-            .rev()
-            .position(|c| c == ']' || c == '}' || c == ')');
-        println!("end_pos is none: {}", end_pos.is_none());
-        return end_pos.is_none();
+    if begin_pos.is_none() {
+        return !has_end_char(string, string.len());
     }
-    // let mut string = string;
-    // loop {
-    //     let bracket = find_bracket(string);
-    //     println!("string: {}    bracket: {}", string, bracket);
 
-    //     if bracket.has_nothing() {
-    //         println!("has_nothing");
-    //         return true;
-    //     }
-    //     if bracket.is_illegal() {
-    //         println!("is_illegal");
-    //         return false;
-    //     }
-    //     string = bracket.remove_brackets(&string);
-    // }
+    let begin_pos = begin_pos.unwrap();
+    if has_end_char(string, begin_pos) {
+        return false;
+    }
+
+    let begin_char = get_char(string, begin_pos);
+    let end_pos = get_end_pos(string, begin_char, begin_pos);
+    if end_pos.is_none() {
+        return false;
+    }
+
+    let end_pos = end_pos.unwrap();
+    let inline_string = &string[begin_pos + 1..end_pos];
+    if !brackets_are_balanced(inline_string) {
+        return false;
+    }
+
+    let rest_string = &string[end_pos + 1..string.len()];
+    return brackets_are_balanced(rest_string);
+}
+
+fn get_char(string: &str, pos: usize) -> char {
+    string.chars().skip(pos).take(1).collect::<Vec<char>>()[0]
+}
+
+fn get_end_char(begin_char: char) -> char {
+    BRACKETS.iter().find(|b| b[0] == begin_char).unwrap()[1]
 }
 
 fn has_end_char(string: &str, end: usize) -> bool {
@@ -65,24 +45,25 @@ fn has_end_char(string: &str, end: usize) -> bool {
         .is_some()
 }
 
-fn get_end_pos(string: &str, begin_char: char, begin_pos: usize, end_char: char) -> Option<usize> {
-    // println!(
-    //     "string, begin_char, begin_pos, end_char: {} {} {} {}",
-    //     string, begin_char, begin_pos, end_char
-    // );
+fn get_end_pos(string: &str, begin_char: char, begin_pos: usize) -> Option<usize> {
+    let end_char = get_end_char(begin_char);
     let mut begin_char_count = 1;
     for i in begin_pos + 1..string.len() {
-        let char = string.chars().nth(i).unwrap();
-        // println!("char: {}", char);
-        if char == begin_char {
-            begin_char_count += 1;
-        } else if char == end_char {
-            begin_char_count -= 1;
-        }
+        let possible_bracket = string.chars().nth(i).unwrap();
+        begin_char_count += count_begin_or_end(possible_bracket, begin_char, end_char);
         if begin_char_count == 0 {
             return Some(i);
         }
     }
-    return None;
-    // let end_pos = string[begin_pos..].chars().position(|c| c == end_char);
+    None
+}
+
+fn count_begin_or_end(possible_bracket: char, begin_char: char, end_char: char) -> i32 {
+    if possible_bracket == begin_char {
+        1
+    } else if possible_bracket == end_char {
+        -1
+    } else {
+        0
+    }
 }
