f252a147040fb791477c010cce827d539aada73a
Akira Kobori <private.beats@gmail.com>
2021-01-19 20:53:10 +0900

    Refactor

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,22 @@
+use std::cmp;
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    if array.is_empty() {
+        return None;
+    }
+
+    let mut first = 0;
+    let mut last = array.len() - 1;
+    while first <= last {
+        let pos = ((last + first) / 2) as usize;
+        match array[pos].cmp(&key) {
+            cmp::Ordering::Equal => return Some(pos),
+            cmp::Ordering::Less => first = pos + 1,
+            cmp::Ordering::Greater => match pos {
+                0 => return None,
+                _ => last = pos - 1,
+            },
+        }
+    }
+    None
 }
