56747b30b7404ae4522c9fe7ca7fdc26ea20d916
Akira Kobori <private.beats@gmail.com>
2021-01-19 20:19:03 +0900

    Submit solution

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
 // This annotation prevents Clippy from warning us that `School` has a
 // `fn new()` with no arguments, but doesn't implement the `Default` trait.
 //
@@ -5,19 +8,37 @@
 // case, we want to keep things relatively simple. The `Default` trait is not the point
 // of this exercise.
 #[allow(clippy::new_without_default)]
-pub struct School {}
+#[derive(Clone)]
+pub struct Student {
+    name: String,
+    grade: u32,
+}
+
+pub struct School {
+    students: Vec<Student>,
+}
 
 impl School {
     pub fn new() -> School {
-        unimplemented!()
+        School { students: vec![] }
     }
 
-    pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!("Add {} to the roster for {}", student, grade)
+    pub fn add(&mut self, grade: u32, name: &str) {
+        self.students.push(Student {
+            grade,
+            name: name.to_string(),
+        });
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+        self.students
+            .iter()
+            .map(|s| s.grade)
+            .collect::<HashSet<u32>>()
+            .iter()
+            .map(|g| *g)
+            .sorted()
+            .collect()
     }
 
     // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
@@ -25,6 +46,11 @@ impl School {
     // the internal structure can be completely arbitrary. The tradeoff is that some data
     // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!("Return the list of students in {}", grade)
+        self.students
+            .iter()
+            .filter(|s| s.grade == grade)
+            .map(|s| s.clone().name)
+            .sorted()
+            .collect()
     }
 }
