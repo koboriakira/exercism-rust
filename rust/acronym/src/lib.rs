1714d4fb8b1fa268c1903b8a714c414b6838b7b7
Akira Kobori <private.beats@gmail.com>
2021-01-19 14:27:00 +0900

    Refactor

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,24 +1,15 @@
 pub fn abbreviate(phrase: &str) -> String {
-    fn get_abbreviate(word: &str) -> Vec<char> {
-        if word.is_empty() {
-            return vec![];
-        };
+    fn get_abbreviate(word: &str) -> String {
         let splited_words = word.split("-").collect::<Vec<&str>>();
         if splited_words.len() > 1 {
             return splited_words
                 .iter()
-                .map(|w| {
-                    if let Some(first_char) = w.chars().nth(0) {
-                        first_char.to_ascii_uppercase()
-                    } else {
-                        ' '
-                    }
-                })
-                .filter(|c| !c.eq(&' '))
-                .collect();
+                .map(|&w| abbreviate(w))
+                .collect::<Vec<String>>()
+                .join("");
         }
         if !word.chars().any(|c| c.is_ascii_lowercase()) {
-            return vec![word.chars().nth(0).unwrap()];
+            return word.chars().nth(0).unwrap().to_string();
         }
         word.chars()
             .filter(|c| !['_', '-'].contains(&c))
@@ -34,6 +25,10 @@ pub fn abbreviate(phrase: &str) -> String {
 
     match phrase.is_empty() {
         true => String::new(),
-        false => phrase.split(" ").map(get_abbreviate).flatten().collect(),
+        false => phrase
+            .split(" ")
+            .map(get_abbreviate)
+            .collect::<Vec<String>>()
+            .join(""),
     }
 }
