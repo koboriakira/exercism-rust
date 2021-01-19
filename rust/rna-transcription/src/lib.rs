3b8372072d23ee5acef272e3a5bf90649deaa374
Akira Kobori <private.beats@gmail.com>
2021-01-19 16:32:50 +0900

    submit solution

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,21 +1,52 @@
 #[derive(Debug, PartialEq)]
-pub struct Dna;
+pub struct Dna {
+    nucleotides: Vec<char>,
+}
 
 #[derive(Debug, PartialEq)]
-pub struct Rna;
+pub struct Rna {
+    nucleotides: Vec<char>,
+}
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        validate(dna, &['A', 'C', 'G', 'T']).and_then(|dna| {
+            Ok(Dna {
+                nucleotides: dna.chars().collect(),
+            })
+        })
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {:?} into corresponding Rna", self);
+        let rna: String = self
+            .nucleotides
+            .iter()
+            .map(|n| match n {
+                'G' => 'C',
+                'C' => 'G',
+                'T' => 'A',
+                'A' => 'U',
+                _ => panic!()
+            })
+            .collect();
+        Rna::new(&rna).ok().unwrap()
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        validate(rna, &['A', 'C', 'G', 'U']).and_then(|rna| {
+            Ok(Rna {
+                nucleotides: rna.chars().collect(),
+            })
+        })
+    }
+}
+
+fn validate<'a>(dna: &'a str, nucleotides: &[char]) -> Result<&'a str, usize> {
+    if let Some(position) = dna.chars().position(|c| !nucleotides.contains(&c)) {
+        Err(position)
+    } else {
+        Ok(dna)
     }
 }
