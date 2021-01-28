c4e8d6a82d1099a3676f0cf7fae667d0bc362cbc
Akira Kobori <private.beats@gmail.com>
2021-01-28 11:26:45 +0900

    Refactor

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,20 +1,11 @@
 use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    parallel(divide_input(input, worker_count))
+    let input_chunks: Vec<&[&str]> = input.chunks(worker_count).collect();
+    parallel(input_chunks)
 }
 
-fn divide_input<'a>(input: &'a [&str], worker_count: usize) -> Vec<Vec<&'a str>> {
-    let mut inputs = (0..worker_count)
-        .map(|_| vec![])
-        .collect::<Vec<Vec<&str>>>();
-    input.iter().enumerate().for_each(|(i, _str)| {
-        inputs[i % worker_count].push(_str);
-    });
-    inputs
-}
-
-fn parallel(inputs: Vec<Vec<&str>>) -> HashMap<char, usize> {
+fn parallel(inputs: Vec<&[&str]>) -> HashMap<char, usize> {
     inputs
         .into_iter()
         .map(|input| crossbeam::scope(|_| analyse(input)))
@@ -26,7 +17,7 @@ fn parallel(inputs: Vec<Vec<&str>>) -> HashMap<char, usize> {
         })
 }
 
-fn analyse(input: Vec<&str>) -> HashMap<char, usize> {
+fn analyse(input: &[&str]) -> HashMap<char, usize> {
     input
         .into_iter()
         .flat_map(|el| el.chars())
