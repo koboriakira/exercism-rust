22f64ba2447fac731a4d3787880ed86a5c3448f9
Akira Kobori <private.beats@gmail.com>
2021-01-28 11:20:31 +0900

    Get faster without locking variables

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,58 +1,38 @@
-use crossbeam;
-use std::{collections::HashMap, sync};
-use sync::Arc;
-use sync::Mutex;
+use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    parallel(input, worker_count)
-    // single(input)
+    parallel(divide_input(input, worker_count))
 }
 
-fn single(input: &[&str]) -> HashMap<char, usize> {
-    let mut result: HashMap<char, usize> = HashMap::new();
-    input.iter().for_each(|i| {
-        sub_frequency(i).iter().for_each(|(c, size)| {
-            *result.entry(*c).or_insert(0) += size;
-        })
+fn divide_input<'a>(input: &'a [&str], worker_count: usize) -> Vec<Vec<&'a str>> {
+    let mut inputs = (0..worker_count)
+        .map(|_| vec![])
+        .collect::<Vec<Vec<&str>>>();
+    input.iter().enumerate().for_each(|(i, _str)| {
+        inputs[i % worker_count].push(_str);
     });
-    result
+    inputs
 }
 
-fn parallel(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let result: Arc<Mutex<HashMap<char, usize>>> = Arc::new(Mutex::new(HashMap::new()));
-    let input = Arc::new(Mutex::new(input.clone().iter()));
-    let _ = crossbeam::scope(|scope| {
-        for _ in 0..worker_count {
-            let result = Arc::clone(&result);
-            let input = Arc::clone(&input);
-            scope.spawn(move |_| {
-                while let Some(el) = input.lock().unwrap().next() {
-                    println!("{}", el);
-                    sub_frequency(el).iter().for_each(|(c, size)| {
-                        *result.lock().unwrap().entry(*c).or_insert(0) += size;
-                        // *result.entry(*c).or_insert(0) += size;
-                    });
-                }
+fn parallel(inputs: Vec<Vec<&str>>) -> HashMap<char, usize> {
+    inputs
+        .into_iter()
+        .map(|input| crossbeam::scope(|_| analyse(input)))
+        .fold(HashMap::new(), |mut map, res| {
+            res.ok().unwrap().into_iter().for_each(|(c, size)| {
+                *map.entry(c).or_insert(0) += size;
             });
-        }
-    });
-    let result = result
-        .lock()
-        .unwrap()
-        .iter()
-        .map(|(c, u)| (*c, *u))
-        .collect::<HashMap<char, usize>>();
-    result
+            map
+        })
 }
 
-fn sub_frequency(input: &str) -> HashMap<char, usize> {
-    let result: HashMap<char, usize> = input
-        .to_lowercase()
-        .chars()
+fn analyse(input: Vec<&str>) -> HashMap<char, usize> {
+    input
+        .into_iter()
+        .flat_map(|el| el.chars())
         .filter(|c| c.is_alphabetic())
         .fold(HashMap::new(), |mut map, c| {
-            *map.entry(c).or_insert(0 as usize) += 1 as usize;
+            *map.entry(c.to_ascii_lowercase()).or_insert(0 as usize) += 1 as usize;
             map
-        });
-    result
+        })
 }
