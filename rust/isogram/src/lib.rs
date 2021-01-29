94f526f123bc0bd76e3e7c2d609b3881dd9f4cf6
Akira Kobori <private.beats@gmail.com>
2021-01-29 21:12:56 +0900

    Submit a solution

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,3 +1,13 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    match candidate
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .collect::<Vec<char>>()
+    {
+        x if x.len() == 0 => true,
+        x => x.len() == x.into_iter().collect::<HashSet<char>>().len(),
+    }
 }
