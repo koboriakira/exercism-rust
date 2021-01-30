71cf8c768c14b3f91a9846b3cd42f8a37df84a59
Akira Kobori <private.beats@gmail.com>
2021-01-30 18:06:04 +0900

    Submit a solution

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,5 +1,11 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+    match s1.len() == s2.len() {
+        true => {
+            let (s1, s2): (Vec<char>, Vec<char>) = (s1.chars().collect(), s2.chars().collect());
+            Some((0..s1.len()).filter(|i| s1[*i] != s2[*i]).count())
+        }
+        false => None,
+    }
 }
