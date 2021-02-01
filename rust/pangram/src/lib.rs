feed4ef1de34bfe31b0022c04a43fe5088c60ca0
Akira Kobori <private.beats@gmail.com>
2021-02-01 10:52:50 +0900

    Submit a solution

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,4 +1,12 @@
+use std::collections::HashSet;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    sentence
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .collect::<HashSet<char>>()
+        .len()
+        == 26
 }
