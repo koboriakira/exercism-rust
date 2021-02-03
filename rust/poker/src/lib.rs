6ae243934e7348270d2c81524eee2e6b67b788d2
Akira Kobori <private.beats@gmail.com>
2021-02-03 23:40:07 +0900

    Refactor

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -2,41 +2,87 @@ use core::panic;
 use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
 
+#[derive(Hash, Eq, PartialEq, PartialOrd, Debug, Clone, Copy)]
+enum Pat {
+    INIT,
+    HighCard(Rank, Rank, Rank, Rank, Rank),
+    OnePair(Rank, Rank, Rank, Rank),
+    TwoPair(Rank, Rank, Rank),
+    ThreeCard(Rank, Rank, Rank),
+    LowestStraight,
+    Straight(Rank),
+    Flush(Rank),
+    FullHouse(Rank, Rank),
+    FourCard(Rank, Rank),
+    LowestStraighFlush,
+    StraightFlush(Rank),
+}
+
+#[derive(Hash, Eq, PartialEq, PartialOrd, Debug, Clone, Copy, Ord)]
+enum Rank {
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    JACK = 11,
+    QUEEN = 12,
+    KING = 13,
+    ACE = 14,
+}
+
+impl Rank {
+    fn new(c: char) -> Self {
+        match c {
+            'A' => Rank::ACE,
+            '2' => Rank::TWO,
+            '3' => Rank::THREE,
+            '4' => Rank::FOUR,
+            '5' => Rank::FIVE,
+            '6' => Rank::SIX,
+            '7' => Rank::SEVEN,
+            '8' => Rank::EIGHT,
+            '9' => Rank::NINE,
+            '1' => Rank::TEN,
+            'J' => Rank::JACK,
+            'Q' => Rank::QUEEN,
+            'K' => Rank::KING,
+            _ => panic!("Illegal char for rank"),
+        }
+    }
+}
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
-    let pat_as_hand = hands
-        .iter()
-        .map(|&hand| {
-            let cards = hand.split(" ").collect::<Vec<&str>>();
-            (analyze(cards), hand)
-        })
-        .fold(HashMap::new(), |mut map, (pat, hand)| {
-            map.entry(pat).or_insert(vec![]).push(hand);
-            map
-        });
-    println!("{:?}", pat_as_hand);
-    let pats = pat_as_hand.keys().map(|x| *x).collect::<Vec<Pat>>();
-    let strongest_pat = strongest(pats);
-    println!("{:?}", strongest_pat);
-    let result = pat_as_hand.get(&strongest_pat).unwrap().clone();
+    let pat_as_hand = hands.iter().fold(HashMap::new(), |mut map, hand| {
+        let pat = analyze(hand);
+        map.entry(pat).or_insert(vec![]).push(*hand);
+        map
+    });
 
-    Some(result)
-}
-
-fn strongest(pats: Vec<Pat>) -> Pat {
-    pats.into_iter()
-        .fold(Pat::INIT, |a, b| match a.partial_cmp(&b) {
+    let strongest_pat = pat_as_hand
+        .keys()
+        .fold(Pat::INIT, |a, b| match a.partial_cmp(b) {
             Some(std::cmp::Ordering::Greater) => a,
             Some(std::cmp::Ordering::Equal) => a,
-            Some(std::cmp::Ordering::Less) => b,
+            Some(std::cmp::Ordering::Less) => *b,
             _ => panic!(""),
-        })
+        });
+
+    Some(pat_as_hand.get(&strongest_pat).unwrap().clone())
 }
 
-fn analyze(cards: Vec<&str>) -> Pat {
+fn analyze(hand: &str) -> Pat {
+    let cards = hand.split(" ").collect::<Vec<&str>>();
+
+    let suited = suits(&cards).len() == 1;
     let rank_as_count = count_rank(&ranks(&cards));
     let two_card = rank_as_count.get(&2);
     let three_card = rank_as_count.get(&3);
@@ -46,7 +92,6 @@ fn analyze(cards: Vec<&str>) -> Pat {
         None => vec![],
     };
     let straight = straight(&kickers);
-    let suited = suits(&cards).len() == 1;
 
     match (four_card, three_card, two_card, straight, suited) {
         (Some(four_card), _, _, _, _) => Pat::FourCard(four_card[0], kickers[0]),
@@ -129,58 +174,4 @@ fn suits(cards: &Vec<&str>) -> HashSet<char> {
         card.chars().nth(card.len() - 1).unwrap()
     }
     cards.into_iter().map(suit).collect::<HashSet<char>>()
-}
-
-#[derive(Hash, Eq, PartialEq, PartialOrd, Debug, Clone, Copy)]
-enum Pat {
-    INIT,
-    HighCard(Rank, Rank, Rank, Rank, Rank),
-    OnePair(Rank, Rank, Rank, Rank),
-    TwoPair(Rank, Rank, Rank),
-    ThreeCard(Rank, Rank, Rank),
-    LowestStraight,
-    Straight(Rank),
-    Flush(Rank),
-    FullHouse(Rank, Rank),
-    FourCard(Rank, Rank),
-    LowestStraighFlush,
-    StraightFlush(Rank),
-}
-
-#[derive(Hash, Eq, PartialEq, PartialOrd, Debug, Clone, Copy, Ord)]
-enum Rank {
-    TWO = 2,
-    THREE = 3,
-    FOUR = 4,
-    FIVE = 5,
-    SIX = 6,
-    SEVEN = 7,
-    EIGHT = 8,
-    NINE = 9,
-    TEN = 10,
-    JACK = 11,
-    QUEEN = 12,
-    KING = 13,
-    ACE = 14,
-}
-
-impl Rank {
-    fn new(c: char) -> Self {
-        match c {
-            'A' => Rank::ACE,
-            '2' => Rank::TWO,
-            '3' => Rank::THREE,
-            '4' => Rank::FOUR,
-            '5' => Rank::FIVE,
-            '6' => Rank::SIX,
-            '7' => Rank::SEVEN,
-            '8' => Rank::EIGHT,
-            '9' => Rank::NINE,
-            '1' => Rank::TEN,
-            'J' => Rank::JACK,
-            'Q' => Rank::QUEEN,
-            'K' => Rank::KING,
-            _ => panic!("Illegal char for rank"),
-        }
-    }
 }
