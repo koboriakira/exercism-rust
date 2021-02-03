21d64f4b3c4504503c4b0d47cf2d4943045eaf2a
Akira Kobori <private.beats@gmail.com>
2021-02-03 15:48:39 +0900

    Submit a solution

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,9 +1,29 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    plain
+        .to_ascii_lowercase()
+        .chars()
+        .filter_map(convert)
+        .collect::<Vec<char>>()
+        .chunks(5)
+        .map(|char_chunk| char_chunk.into_iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    cipher
+        .to_ascii_lowercase()
+        .chars()
+        .filter_map(convert)
+        .collect::<String>()
+}
+
+fn convert(c: char) -> Option<char> {
+    match (c.is_ascii_alphabetic(), c.is_numeric()) {
+        (true, _) => Some((122 - (c as u8) + 97) as char),
+        (_, true) => Some(c),
+        (_, _) => None,
+    }
 }
