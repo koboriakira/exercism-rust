6edbb7ff1d9c13c17135df61cea0fcab10af3bc2
Akira Kobori <private.beats@gmail.com>
2021-02-03 15:31:49 +0900

    Refactor

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,13 +1,8 @@
 use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let input_chunks: Vec<&[&str]> = input.chunks(worker_count).collect();
-    parallel(input_chunks)
-}
-
-fn parallel(inputs: Vec<&[&str]>) -> HashMap<char, usize> {
-    inputs
-        .into_iter()
+    input
+        .chunks(worker_count)
         .map(|input| crossbeam::scope(|_| analyse(input)))
         .fold(HashMap::new(), |mut map, res| {
             res.ok().unwrap().into_iter().for_each(|(c, size)| {
