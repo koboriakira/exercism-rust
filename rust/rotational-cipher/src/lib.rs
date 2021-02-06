f1c90dd40bc9a607939fc11c09000290a558665c
Akira Kobori <private.beats@gmail.com>
2021-02-06 14:19:36 +0900

    Submit a solution

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{}' transform when every letter is shifted using key '{}'?",
-        input,
-        key
-    );
+    let key = key.rem_euclid(26) as u8;
+    input
+        .chars()
+        .map(|c| char_rotate(c, key))
+        .collect::<String>()
+}
+
+fn char_rotate(c: char, key: u8) -> char {
+    if !c.is_alphabetic() {
+        return c;
+    }
+
+    let base_byte = if c.is_lowercase() { b'a' } else { b'A' };
+    (((c as u8) - base_byte + key).rem_euclid(26) + base_byte) as char
 }
