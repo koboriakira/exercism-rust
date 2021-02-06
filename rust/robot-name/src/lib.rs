c8fa62bb8269dd477bf527dd7783415875a6643b
Akira Kobori <private.beats@gmail.com>
2021-02-06 15:49:29 +0900

    Submit a solution

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,15 +1,37 @@
-pub struct Robot;
+use rand::random;
+pub struct Robot {
+    name: String,
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        Self {
+            name: random_name(),
+        }
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        &self.name[..]
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        self.name = random_name();
     }
+}
+
+fn random_name() -> String {
+    fn random_alphabet() -> char {
+        (b'A' + random::<u8>().rem_euclid(26)) as char
+    }
+
+    fn random_number() -> char {
+        (b'0' + random::<u8>().rem_euclid(10)) as char
+    }
+
+    (0..5)
+        .map(|idx| match idx {
+            0 | 1 => random_alphabet(),
+            _ => random_number(),
+        })
+        .collect::<String>()
 }
