d321749e9325bd02c8944ef77776ea50aaf93997
Akira Kobori <private.beats@gmail.com>
2021-02-07 17:48:01 +0900

    Submit a solution

--- rust/simple-cipher/src/lib.rs
+++ rust/simple-cipher/src/lib.rs
@@ -1,82 +1,60 @@
 use rand::Rng;
 
 pub fn encode(key: &str, s: &str) -> Option<String> {
-    if key.is_empty() {
-        return None;
-    }
-    let mut keys = key.chars();
-    let mut characters: Vec<char> = s.chars().rev().collect();
+    validate(key, s)
+        .and_then(|(key, s)| {
+            let mut keys: Vec<char> = key.chars().rev().collect();
+            Ok(s.chars()
+                .map(|c| match keys.pop() {
+                    Some(key) => sub_encode((c, key)),
+                    _ => c,
+                })
+                .collect::<String>())
+        })
+        .ok()
+}
 
-    let mut result: Vec<char> = vec![];
-    while let Some(key) = keys.next() {
-        if !key.is_ascii_alphabetic() || key.is_uppercase() {
-            return None;
-        }
-        if let Some(character) = characters.pop() {
-            if !character.is_ascii_alphabetic() || character.is_uppercase() {
-                return None;
-            }
-            println!("char: {}  key: {}", character, key);
-            result.push(sub_encode((character, key)));
-        } else {
-            break;
-        }
+fn validate<'a>(key: &'a str, s: &'a str) -> Result<(&'a str, &'a str), ()> {
+    if key.is_empty() || s.is_empty() {
+        return Err(());
     }
-
-    if characters.len() > 0 {
-        result.append(&mut characters.into_iter().rev().collect::<Vec<char>>());
+    if !key
+        .chars()
+        .chain(s.chars())
+        .all(|c| c.is_ascii_alphabetic() && c.is_lowercase())
+    {
+        return Err(());
     }
-
-    let encoded_string = result.into_iter().collect::<String>();
-    println!("{}", encoded_string);
-    Some(encoded_string)
+    Ok((key, s))
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
-    if key.is_empty() {
-        return None;
-    }
-    let mut keys = key.chars();
-    let mut characters: Vec<char> = s.chars().rev().collect();
-
-    let mut result: Vec<char> = vec![];
-    while let Some(key) = keys.next() {
-        if !key.is_ascii_alphabetic() || key.is_uppercase() {
-            return None;
-        }
-        if let Some(character) = characters.pop() {
-            if !character.is_ascii_alphabetic() || character.is_uppercase() {
-                return None;
-            }
-            println!("char: {}  key: {}", character, key);
-            result.push(sub_decode((character, key)));
-        } else {
-            break;
-        }
-    }
-
-    if characters.len() > 0 {
-        result.append(&mut characters.into_iter().rev().collect::<Vec<char>>());
-    }
-
-    let encoded_string = result.into_iter().collect::<String>();
-    println!("{}", encoded_string);
-    Some(encoded_string)
+    validate(key, s)
+        .and_then(|(key, s)| {
+            let mut keys: Vec<char> = key.chars().rev().collect();
+            Ok(s.chars()
+                .map(|c| match keys.pop() {
+                    Some(key) => sub_decode((c, key)),
+                    _ => c,
+                })
+                .collect::<String>())
+        })
+        .ok()
 }
 
 pub fn encode_random(s: &str) -> (String, String) {
-    let mut keys = vec![];
-    while keys.len() < 100 {
-        keys.push(random_key());
+    fn random_key() -> char {
+        (rand::thread_rng().gen_range(b'a', b'z' + 1) as u8) as char
     }
-
-    let key = keys.into_iter().collect::<String>();
-
+    fn generate_key() -> String {
+        let mut keys = vec![];
+        while keys.len() < 100 {
+            keys.push(random_key());
+        }
+        keys.into_iter().collect()
+    }
+    let key = generate_key();
     (key.clone(), encode(&key[..], s).unwrap())
-}
-
-fn random_key() -> char {
-    (rand::thread_rng().gen_range(b'a', b'z' + 1) as u8) as char
 }
 
 fn sub_encode((alphabet, key): (char, char)) -> char {
