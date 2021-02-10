09cacf351c02c925e3d892e9d485c3618d4c115d
Akira Kobori <private.beats@gmail.com>
2021-02-10 23:19:55 +0900

    Implement encode function

--- rust/rail-fence-cipher/src/lib.rs
+++ rust/rail-fence-cipher/src/lib.rs
@@ -1,12 +1,44 @@
-pub struct RailFence;
+/// ?.......?
+/// .?.....?.?
+/// ..?...?...?
+/// ...?.?.....?
+/// ....?.......?
+
+pub struct RailFence {
+    rails: usize,
+}
 
 impl RailFence {
     pub fn new(rails: u32) -> RailFence {
-        unimplemented!("Construct a new fence with {} rails", rails)
+        Self {
+            rails: rails as usize,
+        }
     }
 
     pub fn encode(&self, text: &str) -> String {
-        unimplemented!("Encode this text: {}", text)
+        let init: Vec<Vec<char>> = vec![];
+        let result = text.chars().fold(
+            (init, 0 as usize, 1 as isize),
+            |(mut acc, idx, next), ch| {
+                match acc.get_mut(idx) {
+                    Some(char_list) => char_list.push(ch),
+                    None => acc.push(vec![ch]),
+                }
+                if idx == 0 && next == -1 {
+                    (acc, 1, 1)
+                } else if (idx as isize + next) as usize == self.rails {
+                    (acc, idx - 1, -1)
+                } else {
+                    (acc, (idx as isize + next) as usize, next)
+                }
+            },
+        );
+        println!("{:?}", result.0);
+
+        result.0.into_iter().fold(String::new(), |mut acc, result| {
+            acc.push_str(&result.into_iter().collect::<String>());
+            acc
+        })
     }
 
     pub fn decode(&self, cipher: &str) -> String {
