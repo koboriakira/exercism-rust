d17d807c31349f885d1e6dbe51a4c2114b392f88
Akira Kobori <private.beats@gmail.com>
2021-02-12 13:48:01 +0900

    Refactor

--- rust/forth/src/lib.rs
+++ rust/forth/src/lib.rs
@@ -18,7 +18,7 @@ pub enum Error {
     InvalidWord,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 enum Element {
     Number(Value),
     Add,
@@ -29,9 +29,25 @@ enum Element {
     Drop,
     Swap,
     Over,
-    StartBuildingBuiltIn,
-    EndBuildingBuiltIn,
     BuiltIn(Vec<Element>),
+}
+
+impl Element {
+    fn needs_one_argument(&self) -> bool {
+        [Element::Duplicate, Element::Drop].contains(self)
+    }
+
+    fn needs_two_arguments(&self) -> bool {
+        [
+            Element::Add,
+            Element::Subtract,
+            Element::Multiply,
+            Element::Divide,
+            Element::Swap,
+            Element::Over,
+        ]
+        .contains(self)
+    }
 }
 
 impl Forth {
@@ -49,151 +65,119 @@ impl Forth {
     }
 
     pub fn eval(&mut self, input: &str) -> ForthResult {
-        let result = input
+        input
             .to_lowercase()
             .split(" ")
             .into_iter()
             .try_for_each(|el| {
                 println!("word: {}", el);
-                if self.mode_build_builtin {
-                    match ";".eq(el) {
-                        false => {
-                            self.builtin_stack.push(el.to_string());
-                            Ok(())
-                        }
-                        true => {
-                            let command_name = self.builtin_stack.remove(0);
-                            if convert_to_integer(&command_name).is_ok() {
-                                return Err(Error::InvalidWord);
-                            }
-                            let result = self
-                                .builtin_stack
-                                .clone()
-                                .into_iter()
-                                .enumerate()
-                                .try_for_each(|(idx, el)| match self.evaluate(&el) {
-                                    Ok(element) => {
-                                        println!("Buildin Element: {:?}", element);
-                                        if idx == 0 {
-                                            self.builtins
-                                                .entry(command_name.clone())
-                                                .or_insert_with(|| vec![])
-                                                .clear();
-                                        }
+                match (self.mode_build_builtin, el) {
+                    (true, ":") | (false, ";") => Err(Error::InvalidWord),
+                    // start to create builtin
+                    (false, ":") => {
+                        self.mode_build_builtin = true;
+                        Ok(())
+                    }
+                    // finish to create builtin
+                    (true, ";") => self.get_command_name().and_then(|command_name| {
+                        self.builtin_stack
+                            .clone()
+                            .into_iter()
+                            .enumerate()
+                            .try_for_each(|(idx, el)| match self.evaluate(&el) {
+                                Ok(element) => {
+                                    println!("Buildin Element: {:?}", element);
+                                    if idx == 0 {
                                         self.builtins
                                             .entry(command_name.clone())
                                             .or_insert_with(|| vec![])
-                                            .push(element);
-                                        Ok(())
+                                            .clear();
                                     }
-                                    Err(err) => Err(err),
-                                });
-                            self.mode_build_builtin = false;
-                            self.builtin_stack.clear();
-                            println!("Build built-in function: {:?}", self.builtins);
-                            result
-                        }
+                                    self.builtins
+                                        .entry(command_name.clone())
+                                        .or_insert_with(|| vec![])
+                                        .push(element);
+                                    Ok(())
+                                }
+                                Err(err) => Err(err),
+                            })
+                            .and_then(|_| {
+                                self.mode_build_builtin = false;
+                                self.builtin_stack.clear();
+                                println!("Build built-in function: {:?}", self.builtins);
+                                Ok(())
+                            })
+                    }),
+                    // stack for builtin
+                    (true, el) => {
+                        self.builtin_stack.push(el.to_string());
+                        Ok(())
                     }
-                } else {
-                    match self.evaluate(el) {
-                        Ok(element) => self.execute(element),
-                        Err(err) => Err(err),
-                    }
+                    // execute
+                    (false, el) => self.evaluate(el).and_then(|element| self.execute(element)),
                 }
-            });
-        if self.mode_build_builtin {
+            })
+            .and_then(|_| match self.mode_build_builtin {
+                true => Err(Error::InvalidWord),
+                _ => Ok(()),
+            })
+    }
+
+    fn get_command_name(&mut self) -> Result<String, Error> {
+        let command_name = self.builtin_stack.remove(0);
+        if convert_to_integer(&command_name).is_ok() {
             return Err(Error::InvalidWord);
         }
-        result
+        return Ok(command_name);
     }
 
     fn execute(&mut self, element: Element) -> Result<(), Error> {
         println!("Execute: {:?} => {:?}", self.stack, element);
         match element {
-            Element::StartBuildingBuiltIn => match self.mode_build_builtin {
-                true => Err(Error::InvalidWord),
-                false => {
-                    self.mode_build_builtin = true;
-                    Ok(())
-                }
-            },
-            Element::EndBuildingBuiltIn => match self.mode_build_builtin {
-                false => Err(Error::InvalidWord),
-                true => {
-                    self.mode_build_builtin = false;
-                    Ok(())
-                }
-            },
             Element::Number(num) => {
                 self.stack.push(num);
                 Ok(())
             }
-            Element::Add => match (self.stack.pop(), self.stack.pop()) {
-                (Some(b), Some(a)) => {
-                    self.stack.push(a + b);
-                    Ok(())
-                }
-                _ => Err(Error::StackUnderflow),
-            },
-            Element::Subtract => match (self.stack.pop(), self.stack.pop()) {
-                (Some(b), Some(a)) => {
-                    self.stack.push(a - b);
-                    Ok(())
-                }
-                _ => Err(Error::StackUnderflow),
-            },
-            Element::Multiply => match (self.stack.pop(), self.stack.pop()) {
-                (Some(b), Some(a)) => {
-                    self.stack.push(a * b);
-                    Ok(())
-                }
-                _ => Err(Error::StackUnderflow),
-            },
-            Element::Divide => match (self.stack.pop(), self.stack.pop()) {
-                (Some(0), Some(_)) => Err(Error::DivisionByZero),
-                (Some(b), Some(a)) => {
-                    self.stack.push(a / b);
-                    Ok(())
-                }
-                _ => Err(Error::StackUnderflow),
-            },
-            Element::Duplicate => match &self.stack.clone().last() {
-                Some(val) => {
-                    self.stack.push(*val.clone());
-                    Ok(())
-                }
-                _ => Err(Error::StackUnderflow),
-            },
-            Element::Drop => match self.stack.pop() {
-                Some(_) => Ok(()),
-                _ => Err(Error::StackUnderflow),
-            },
-            Element::Swap => match (self.stack.pop(), self.stack.pop()) {
-                (Some(b), Some(a)) => {
-                    self.stack.push(b);
-                    self.stack.push(a);
-                    Ok(())
-                }
-                _ => Err(Error::StackUnderflow),
-            },
-            Element::Over => match (self.stack.pop(), self.stack.pop()) {
-                (Some(b), Some(a)) => {
-                    self.stack.push(a);
-                    self.stack.push(b);
-                    self.stack.push(a);
-                    Ok(())
-                }
-                _ => Err(Error::StackUnderflow),
-            },
             Element::BuiltIn(elements) => elements
                 .into_iter()
                 .try_for_each(|element| self.execute(element.clone())),
-            _ => Ok(()),
+            element if element.needs_one_argument() => {
+                match (element, self.stack.pop()) {
+                    (Element::Duplicate, Some(val)) => {
+                        self.stack.push(val);
+                        self.stack.push(val);
+                    }
+                    (Element::Drop, Some(_)) => {}
+                    _ => return Err(Error::StackUnderflow),
+                };
+                Ok(())
+            }
+            element if element.needs_two_arguments() => {
+                match (element, self.stack.pop(), self.stack.pop()) {
+                    (Element::Add, Some(b), Some(a)) => self.stack.push(a + b),
+                    (Element::Subtract, Some(b), Some(a)) => self.stack.push(a - b),
+                    (Element::Multiply, Some(b), Some(a)) => self.stack.push(a * b),
+                    (Element::Divide, Some(0), Some(_)) => return Err(Error::DivisionByZero),
+                    (Element::Divide, Some(b), Some(a)) => self.stack.push(a / b),
+                    (Element::Swap, Some(b), Some(a)) => {
+                        self.stack.push(b);
+                        self.stack.push(a);
+                    }
+                    (Element::Over, Some(b), Some(a)) => {
+                        self.stack.push(a);
+                        self.stack.push(b);
+                        self.stack.push(a);
+                    }
+                    _ => return Err(Error::StackUnderflow),
+                }
+                Ok(())
+            }
+            _ => panic!(""),
         }
     }
 
     fn evaluate(&self, el: &str) -> Result<Element, Error> {
-        if let Some(elements) = self.builtins.get(&el.to_lowercase()) {
+        if let Some(elements) = self.builtins.get(el) {
             return Ok(Element::BuiltIn(elements.clone()));
         }
 
@@ -206,8 +190,6 @@ impl Forth {
             "drop" => return Ok(Element::Drop),
             "swap" => return Ok(Element::Swap),
             "over" => return Ok(Element::Over),
-            ":" => return Ok(Element::StartBuildingBuiltIn),
-            ";" => return Ok(Element::EndBuildingBuiltIn),
             _ => {}
         }
 
