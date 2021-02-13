2f073642e377e266970e4f6395d3cb6a51966af8
Akira Kobori <private.beats@gmail.com>
2021-02-13 12:48:39 +0900

    Submit a solution

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,6 +1,14 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    collatz_recurcively(n, 0)
+}
+
+fn collatz_recurcively(n: u64, cnt: u64) -> Option<u64> {
+    match n {
+        0 => None,
+        1 => Some(cnt),
+        n => match n % 2 == 0 {
+            true => collatz_recurcively(n / 2, cnt + 1),
+            _ => collatz_recurcively(3 * n + 1, cnt + 1),
+        },
+    }
 }
